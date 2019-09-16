ceb234e86411feac7092d0f1bb01b843c53321ae
Natsu <kirito.flamedragon@gmail.com>
2019-09-16 14:25:24 -0500

    Adding all the new files for first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(function(){
+
+
+    $('.menu-btn').on('click', ()=>{
+        $('.side-menu').css('width', '230px');
+    })
+
+    $('.close-btn').on('click', () =>{
+        $('.side-menu').css('width', '0');
+    })
+
+// END OF JQUERY FUNCTION
+});

2203a46d212b9937f82ce8413cf4cf1d49cfc1d8
Natsu <kirito.flamedragon@gmail.com>
2019-09-24 02:05:59 -0500

    added popular watch section, jquery like button changes and shopping cart add numbers

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 $(function(){
-
+    // Menu Slide out
+    var sideMenu = $('.side-menu');
 
     $('.menu-btn').on('click', ()=>{
-        $('.side-menu').css('width', '230px');
+        sideMenu.css('width', '230px');
     })
 
     $('.close-btn').on('click', () =>{
-        $('.side-menu').css('width', '0');
+        sideMenu.css('width', '0');
+    })
+
+    // Fav like heart
+    $('i.like-icon').on('click', function(){
+        $(this).toggleClass('active');
+    })
+
+    // Clicking the Shopping cart adds number to cart
+    var count = 0;
+    $('i.cart-icon').on('click', function(){
+        count ++;
+        $('span.cart-num').text(count);
     })
 
 // END OF JQUERY FUNCTION
